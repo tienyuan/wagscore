3d1dacc6500b172aa031c49ff1aada1bfd2f5c71
Tien Yuan <tien.yuan@gmail.com>
2014-11-21 15:06:52 +0000

    passing js in progress

--- app/assets/javascripts/clientside_geocoder.js
+++ app/assets/javascripts/clientside_geocoder.js
@@ -8,10 +8,16 @@ function codeAddress() {
   var address = document.getElementById('address').value;
   client_geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
-      alert(results[0].geometry.location);
+      console.log(results)
+      $('#search_location_lat').val(location.lat());
+      alert(results[0].geometry.location.lat()); #put this into .val for hidden field
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
   });
 }
 
+function geocodeAddressAndSubmit() {
+  codeAddress()
+
+}
