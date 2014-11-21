e88ca1b92362b7f8841a8ac42f3d76d77aa2e992
Tien Yuan <tien.yuan@gmail.com>
2014-11-21 15:33:01 +0000

    moved calculate_score into new class

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -46,38 +46,7 @@ RSpec.describe Location, :type => :model do
     it "returns all locations near a search_term when admin_view is true" do
       expect(Location.search_and_show(@public_location.full_address, 10, true)).to contain_exactly(@public_location, @private_location)
     end
-  end
-
-  describe ".calculate_score" do
-
-    #so it's best if the test works regardless of what categories are present
-    #let's say the ideal area has one of each category
-    #there are 4 categories: park, vet, store, groomer
-    #so each category is worth 25%
-    #i'd want to count the number of unique categories present given a list of locations then 
-    #and multiply by the value per category
-    #remember, each location can have more than one category...
-
-    before do
-      @category_1 = create(:category)
-      @category_2 = create(:category)
-      @category_3 = create(:category)
-      @category_4 = create(:category)
-      @location_1 = create(:location)
-      @location_2 = create(:location)
-      @location_3 = create(:location)
-      @categorization = create(:categorization, category: @category_1, location: @location_1)
-      @categorization = create(:categorization, category: @category_2, location: @location_2)
-      @categorization = create(:categorization, category: @category_3, location: @location_3)
-      @categorization = create(:categorization, category: @category_4, location: @location_3)
-    end
-    
-    it "calculates a score given a list of locations" do
-      expect([@location_1, @location_2].calculate_score).to eq("50%")
-      expect([@location_2, @location_3].calculate_score).to eq("75%")
-      expect([@location_1, @location_2, @location_3].calculate_score).to eq("100%")
-    end
-  end
+  end  
 
   describe "ActiveModel validations" do
     before do
