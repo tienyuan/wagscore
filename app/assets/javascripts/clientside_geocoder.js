b0d89b80716c232408fc32ac867464cae95ab04b
Tien Yuan <tien.yuan@gmail.com>
2014-11-22 17:43:54 +0000

    fixed clientside submit

--- app/assets/javascripts/clientside_geocoder.js
+++ app/assets/javascripts/clientside_geocoder.js
@@ -4,19 +4,15 @@ function initialize() {
   client_geocoder = new google.maps.Geocoder();
 }
 
-function geocodeAddress() {
+function geocodeAddressThenSubmit() {
   var address = document.getElementById('address').value;
   client_geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       $('#search_location_lat').val(results[0].geometry.location.lat());
       $('#search_location_lng').val(results[0].geometry.location.lng());
+      $('#search_form').submit();
     } else {
-      alert('Geocode was not successful for the following reason: ' + status);
+      alert('Search was not successful for the following reason: ' + status);
     }
   });
-}
-
-function geocodeAddressAndSubmit() {
-  geocodeAddress();
-  $('#search_form').submit();
 }
